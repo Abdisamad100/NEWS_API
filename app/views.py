67f59233003c1b71d6be73bb776208c0f6fd901a
abdisamad100 <abdisamadcade004@gmail.com>
2020-11-29 15:24:53 +0300

    add macros file

--- app/views.py
+++ app/views.py
@@ -4,15 +4,30 @@ from .request import get_news
 
 
 # Views
-@app.route('/')
+# @app.route('/')
+# def index():
+
+#     '''
+#     View root page function that returns the index page and its data
+#     '''
+
+#     # Getting popular movie
+#     latest_news = get_news('latest')
+#     print(latest_news)
+#     title = 'Home - Welcome to The best Movie Review Website Online'
+#     return render_template('index.html', title = title,latest = latest_news)
+
+@app.route('/', methods=['GET'])
 def index():
 
     '''
-    View root page function that returns the index page and its data
+    view news page function that returns the news details  and its data
     '''
-
-    # Getting popular movie
-    news = get_news()
     
-    title = 'News Headlines'
-    return render_template('index.html', title = title, news = news)
+    news = get_news()
+    # print(news)
+    title = "News_Highlight"
+    
+    return render_template('index.html', news = news, title=title)
+    
+    
