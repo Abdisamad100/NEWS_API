d0302135cc0bb282509ead389f3d40a9a66edcbe
abdisamad100 <abdisamadcade004@gmail.com>
2020-11-29 19:49:38 +0300

    add function processes news article

--- app/request.py
+++ app/request.py
@@ -61,3 +61,34 @@ def process_source(news_list):
         news_source.append(news_object)
 
     return news_source
+
+
+
+
+
+def process_article(news_list):
+    """
+    Function  that processes the news article and transform them to a list of Objects
+    Args:
+        news_list: A list of dictionaries that contain news details
+    Returns :
+        news_source: A list of news objects
+    """
+    news_article = []
+    for news_item in news_list:
+        id = news_item.get("id")
+        author = news_item.get("author")
+        title = news_item.get("title")
+        description = news_item.get("description")
+        url = news_item.get("url")
+        urlToImage = news_item.get("urlToImage")
+        publishedAt = news_item.get("publishedAt")
+        content = news_item.get("content")
+
+        if urlToImage:
+            news_object = NewsArticle(
+                id, author,title,description,url,urlToImage,publishedAt,content
+            )
+            news_article.append(news_object)
+
+    return news_article
